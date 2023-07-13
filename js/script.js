cbf0fe5a84509a9298fad39641f30805e9c45846
ostap <127669847+ostapzat@users.noreply.github.com>
2023-07-13 15:02:05 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,9 +20,51 @@ function closeModal() {
 }
 
 function sendMail() {
+    // Get the input values
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+
+    // Get the error message elements
+    var nameError = document.getElementById("nameError");
+    var emailError = document.getElementById("emailError");
+    var passwordError = document.getElementById("passwordError");
+
+    // Reset previous error messages
+    nameError.textContent = "";
+    emailError.textContent = "";
+    passwordError.textContent = "";
+
+    // Validate name field
+    if (nameInput.value.trim() === "") {
+        nameError.textContent = "Name is required";
+        nameInput.focus();
+        return;
+    }
+
+    // Validate email field
+    if (emailInput.value.trim() === "") {
+        emailError.textContent = "Email is required";
+        emailInput.focus();
+        return;
+    } else if (!isValidEmail(emailInput.value.trim())) {
+        emailError.textContent = "Invalid email format";
+        emailInput.focus();
+        return;
+    }
+
+    // Validate password field
+    if (passwordInput.value.trim() === "") {
+        passwordError.textContent = "Password is required";
+        passwordInput.focus();
+        return;
+    }
+
+    // Proceed with sending the email
     var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
+        name: nameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value
     };
 
     const serviceID = "service_w3d8921";
@@ -30,10 +72,50 @@ function sendMail() {
 
     emailjs.send(serviceID, templateID, params)
         .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
+            // Reset the form
+            nameInput.value = "";
+            emailInput.value = "";
+            passwordInput.value = "";
+
             console.log(res);
         })
         .catch(err => console.log(err));
-
 }
+
+function isValidEmail(email) {
+    // Simple email format validation using regex
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+
+
+// Get the button:
+function scrollToTop() {
+    var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentPosition > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, currentPosition - currentPosition / 10);
+    }
+}
+
+window.addEventListener("scroll", function () {
+    var scrollHeight = document.documentElement.scrollHeight;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var windowHeight = document.documentElement.clientHeight;
+
+    if (scrollTop > (scrollHeight / 3)) {
+        document.getElementById("scrollUpButton").style.display = "flex";
+    } else {
+        document.getElementById("scrollUpButton").style.display = "none";
+    }
+});
+
+document.getElementById("scrollUpButton").addEventListener("click", function () {
+    scrollToTop();
+});
+
+
+
+new WOW().init();
